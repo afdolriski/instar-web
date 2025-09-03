75ebba7fea070d10653bba667d356693c3f6a774
Afdol Rizki Halim <afdolriski@gmail.com>
2025-09-03 16:21:48 +0700

    use ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import Image from "next/image";
 import { Montserrat, Merriweather } from "next/font/google";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
@@ -37,7 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-TR8G53WZ" />
+      <GoogleAnalytics gaId="G-47XK9LY0NP" />
       <body
         className={`${monsterratFont.variable} ${merriWeatherFont.variable} font-sans antialiased`}
       >
